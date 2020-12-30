3dbc8386673f9376c41d2e4d7753bf7e29753f32
Avinash Sinha <savinash2608@gmail.com>
2020-12-30 20:00:18 +0530

    back to normal

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,26 @@
-// import { ApolloServer } from "apollo-server-express";
-// import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
 import express from "express";
-// import "reflect-metadata";
-// import { createConnection } from "typeorm";
-// import { apolloConfig, corsConfig, typeormConfig } from "./config";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { apolloConfig, corsConfig, typeormConfig } from "./config";
 import { PORT, __prod__ } from "./constants";
-// import { router } from "./rest";
+import { router } from "./rest";
 
 const main = async () => {
-  // await createConnection(typeormConfig);
+  await createConnection(typeormConfig);
   const app = express();
 
-  app.get(
-    "/.well-known/acme-challenge/VeX5Ry7AHsmfkmh4zvwzM5QkzNkxG481uZtxAXvMxV4",
-    (_, res) =>
-      res.send(
-        "VeX5Ry7AHsmfkmh4zvwzM5QkzNkxG481uZtxAXvMxV4.8OagZ1r5d5ryybc2ain9wkfU-s-9M8T4zELxEtMscQI"
-      )
-  );
+  app.use(cors(corsConfig));
 
-  // app.use(cors(corsConfig));
+  app.use(router);
 
-  // app.use(router);
+  const apolloServer = new ApolloServer(await apolloConfig());
 
-  // const apolloServer = new ApolloServer(await apolloConfig());
-
-  // apolloServer.applyMiddleware({
-  //   app,
-  //   cors: false,
-  // });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(PORT, () => {
     console.log(`The server has started on port ${PORT}.`);
