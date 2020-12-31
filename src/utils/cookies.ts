6ac864d9be12b79bc67472f1f2530dc17a4978de
Avinash Sinha <savinash2608@gmail.com>
2020-12-31 15:14:29 +0530

    httpOnly = false

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -7,13 +7,11 @@ export const setTokens = (
   refreshToken: string
 ) => {
   res.cookie(ACCESS_TOKEN, accessToken, {
-    httpOnly: __prod__,
     secure: __prod__,
     sameSite: __prod__ && "none",
     maxAge: 1000 * 60, // 1 minute
   });
   res.cookie(REFRESH_TOKEN, refreshToken, {
-    httpOnly: __prod__,
     secure: __prod__,
     sameSite: __prod__ && "none",
     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
@@ -22,12 +20,10 @@ export const setTokens = (
 
 export const clearTokens = (res: Response) => {
   res.clearCookie(ACCESS_TOKEN, {
-    httpOnly: __prod__,
     secure: __prod__,
     sameSite: __prod__ && "none",
   });
   res.clearCookie(REFRESH_TOKEN, {
-    httpOnly: __prod__,
     secure: __prod__,
     sameSite: __prod__ && "none",
   });
